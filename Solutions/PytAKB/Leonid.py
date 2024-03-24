f9400783510e9efd10a03381092fb186a9f39fc5
Leonid Kokorev <leotechru@ya.ru>
2024-03-24 23:36:11 +0300

    Update Leonid.py

--- Solutions/PytAKB/Leonid.py
+++ Solutions/PytAKB/Leonid.py
@@ -1,7 +1,8 @@
 import telebot
 import random
 import requests
-from token import TOKEN 
+from token import TOKEN
+from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -9,16 +10,34 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     bot.send_message(message.chat.id, "Привет")
 
-@bot.message_handler(commands=["coffee"])
-def coffee(message): 
-    r = requests.get("https://coffee.alexflipnote.dev/random.json").json()
-    img = r["file"]
-    bot.send_photo(message.chat.id, img)
+# Список приветствий
+greetings = ['привет', 'добрый день', 'добрый вечер', 'доброе утро', 'здравствуйте']
+
+@bot.message_handler(func=lambda message: message.text.lower() in greetings)
+def greet(message):
+    name = message.from_user.first_name
+    bot.reply_to(message, f'Здравствуй, {name}')
+
+@bot.message_handler(commands=['reverse'])
+def reverse_command(message):
+    msg = bot.reply_to(message, "Введите текст, который нужно перевернуть:")
+    bot.register_next_step_handler(msg, reverse_text)
+
+def reverse_text(message):
+    text = message.text
+    reversed_text = text[::-1]
+    bot.reply_to(message, reversed_text)
 
 #@bot.message_handler(func = lambda x: "/rand" in x.text)
 @bot.message_handler(commands=["rand"])
 def send_random(message):
     bot.send_message(message.chat.id, f"случайное число - {random.randint(0, 100)}") 
+
+@bot.message_handler(commands=["coffee"])
+def coffee(message): 
+    r = requests.get("https://coffee.alexflipnote.dev/random.json").json()
+    img = r["file"]
+    bot.send_photo(message.chat.id, img)
 
 @bot.message_handler(content_types = ["text"])
 def echo(message):
