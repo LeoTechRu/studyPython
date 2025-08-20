13fd891920ccc8aa479c912684fef2520ba786c1
Leonid Kokorev <leotechru@gmail.com>
2025-08-20 17:17:34 +0300

    deleted:    PythonTest/Day1/Practice/strong_password.py 	deleted:    PythonTest/Day1/Solutions/sol_strong_password_v1.py 	deleted:    PythonTest/Day1/Solutions/sol_strong_password_v2.py 	deleted:    PythonTest/Day1/Solutions/sol_strong_password_v3.py 	deleted:    PythonTest/Day1/__init__.py 	deleted:    PythonTest/Day1/example.py 	deleted:    PythonTest/Day1/unittest_examples/__init__.py 	deleted:    PythonTest/Day1/unittest_examples/calc.py 	deleted:    PythonTest/Day1/unittest_examples/calc_test.py 	deleted:    PythonTest/Day2/Practice/encode_decode.py 	deleted:    PythonTest/Day2/Solutions/__init__.py 	deleted:    PythonTest/Day2/Solutions/decrypt.py 	deleted:    PythonTest/Day2/Solutions/encode_decode.py 	deleted:    PythonTest/Day2/Solutions/encode_decode_test.py 	deleted:    PythonTest/Day2/Solutions/sol_strong_password_v1.py 	deleted:    PythonTest/Day2/Solutions/sol_strong_password_v1_test.py 	deleted:    PythonTest/Day2/Solutions/test_decrypt.py 	deleted:    PythonTest/Day2/__init__.py 	deleted:    PythonTest/Day2/global_nonlocal_ex.py 	deleted:    PythonTest/Day2/more_calc.py 	deleted:    PythonTest/Day2/more_calc_test.py 	deleted:    PythonTest/Day2/run_tests.py 	deleted:    PythonTest/Day2/subtests_test.py 	deleted:    PythonTest/Day3/Solutions/codec26.py 	deleted:    PythonTest/Day3/Solutions/sol_encode_decode.py 	deleted:    PythonTest/Day3/Solutions/test_codec26.py 	deleted:    PythonTest/Day3/pytest_materials/Exercise/fixture_practice.md 	deleted:    PythonTest/Day3/pytest_materials/Lection1/Lection_1.md 	deleted:    PythonTest/Day3/pytest_materials/Lection1/test_one.py 	deleted:    PythonTest/Day3/pytest_materials/Lection1/test_two.py 	deleted:    PythonTest/Day3/pytest_materials/Lection2/Lection2.md 	deleted:    PythonTest/Day3/pytest_materials/Lection2/__init__.py 	deleted:    PythonTest/Day3/pytest_materials/Lection2/test_classes.py 	deleted:    PythonTest/Day3/pytest_materials/Lection2/test_exceptions.py 	deleted:    PythonTest/Day3/pytest_materials/Lection2/test_structure.py 	deleted:    PythonTest/Day3/pytest_materials/Lection2/test_task.py 	deleted:    PythonTest/Day3/pytest_materials/Lection2/test_task_mod.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/Lection3.md 	deleted:    PythonTest/Day3/pytest_materials/Lection3/__init__.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir1/__init__.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir1/conftest.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir1/test_subdir_count.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir1/test_three.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir2/conftest.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir2/test_subdir_count.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/subdir2/test_three.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/test_count.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/test_count_initial.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/test_fixtures.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/test_mod_scope.py 	deleted:    PythonTest/Day3/pytest_materials/Lection3/test_rename_fixture.py 	deleted:    PythonTest/Day3/pytest_materials/__init__.py 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/README.md 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/pyproject.toml 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/src/tasks/__init__.py 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/src/tasks/api.py 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/src/tasks/cli.py 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/src/tasks/db.py 	deleted:    PythonTest/Day3/pytest_materials/tasks_project/tasks_store/tasks_db.json

--- PythonTest/Day3/pytest_materials/Lection3/subdir2/test_three.py
+++
@@ -1,7 +0,0 @@
-import tasks
-
-def test_three(tasks_db):
-    tasks_db.add_task(tasks.Task("first"))
-    tasks_db.add_task(tasks.Task("second"))
-    tasks_db.add_task(tasks.Task("third"))
-    assert tasks_db.count() == 3
--- PythonTest/Day3/pytest_materials/tasks_project/src/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Top-level package for tasks."""
-
-__version__ = "1.0.0"
-
-from .api import *  # noqa
-from .cli import app  # noqa
